ef3d8eb8a0fe6bd3b7169fb92ef594b4912f0c0d
Fabio Christiano <fabio.augusto.christiano@gmail.com>
2018-04-25 20:50:28 -0300

    Created spring boot project

---
+++ calc-deployment-time/src/main/java/br/com/fchristiano/calcdeploymenttime/CalcDeploymentTimeApplication.java
@@ -0,0 +1,12 @@
+package br.com.fchristiano.calcdeploymenttime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcDeploymentTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcDeploymentTimeApplication.class, args);
+	}
+}
